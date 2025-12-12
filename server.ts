1ee49028921258768ed4c579bdd2acf1dd40ede0
Kristjan Jansen <Kristjan.Jansen@enefit.ee>
2025-12-12 14:51:20 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,6 +1,9 @@
 export default {
   async fetch(): Promise<Response> {
-    const body = { message: "Hello world!" };
+    const body = {
+      message: "Hello world!",
+      timestamp: new Date().toISOString(),
+    };
     return new Response(JSON.stringify(body), {
       headers: { "Content-Type": "application/json" },
     });
