2aafef53b5a496e49435ae3931b24304e413b0ed
Kristjan Jansen <Kristjan.Jansen@enefit.ee>
2025-12-14 15:57:21 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,26 +1,28 @@
-const response = {
-  data: [
-    {
-      id: 1,
-      title: "First title",
-    },
-    {
-      id: 2,
-      title: "Second title",
-    },
-  ],
-  meta: {
-    timestamp: new Date().toISOString(),
-    env: {
-      BILLING_BACKEND_URL: process.env.BILLING_BACKEND_URL,
-    },
-  },
-};
-
 export default {
   async fetch(): Promise<Response> {
-    return new Response(JSON.stringify(response), {
-      headers: { "Content-Type": "application/json" },
-    });
+    return new Response(
+      JSON.stringify({
+        data: [
+          {
+            id: 1,
+            title: "First title",
+          },
+          {
+            id: 2,
+            title: "Second title",
+          },
+        ],
+        meta: {
+          timestamp: new Date().toISOString(),
+          env: {
+            BILLING_BACKEND_URL2: "test",
+            BILLING_BACKEND_URL: process.env.BILLING_BACKEND_URL,
+          },
+        },
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   },
 };
